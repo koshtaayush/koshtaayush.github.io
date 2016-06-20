385e8bb848257f2acd2c6f7c053ad548575e080d
koshtaayush <koshtaayush@gmail.com>
2016-06-20 22:15:06 +0530

    customization
    added css and images

---
+++ myscript.js
@@ -0,0 +1,11 @@
+function myFunc(val){
+  if(val==1){
+  window.location.href="www.gmail.com";
+  }
+  if(val==2){
+  window.location="www.facebook.com";
+  }
+  if(val==3){
+  window.location="www.twitter.com";
+  }
+    }
